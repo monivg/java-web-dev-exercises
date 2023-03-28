bfb52fdcc39580f629b4b71e1f5d6c58b7148735
monivg <monicav2013@gmail.com>
2023-03-27 19:57:57 -0500

    completed studio

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius: ");
+        System.out.println("Enter a radius:   ");
         Double radius = input.nextDouble();
 
         Double circleArea = Circle.getArea(radius);
