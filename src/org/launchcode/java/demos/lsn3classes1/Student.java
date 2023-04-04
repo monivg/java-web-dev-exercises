282700970f9fa363a27f8b8df1637cba850881cf
monivg <monicav2013@gmail.com>
2023-04-03 19:17:39 -0500

    completed exercises 1 & 2

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -6,8 +6,33 @@ package org.launchcode.java.demos.lsn3classes1;
 public class Student {
 
     private String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
     private int studentId;
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
     private int numberOfCredits = 0;
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
     private double gpa = 0.0;
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    public double getGpa() {
+        return gpa;
+    }
 
 }
